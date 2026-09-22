0bd165e8bd490f4cfb961bff92cc98dd34f57f8e
Matt <matthewjstephens@gmail.com>
2026-09-22 12:19:04 -0500

    removed previous change

--- src/gate_boron.cpp
+++ src/gate_boron.cpp
@@ -9,8 +9,6 @@
 #include "Particle.h"
 #include "application.h"
 #include "BeaconScanner.h"
-PRODUCT_VERSION(1);
-PRODUCT_ID(45700);
 
 // Let Device OS manage the connection to the Particle Cloud
 SYSTEM_MODE(AUTOMATIC);
